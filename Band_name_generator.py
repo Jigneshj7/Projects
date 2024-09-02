7ef34f7127931dae3accee4316f90500d4ee6e5b
Jigneshj7 <jigneshsharmaj7@gmail.com>
2024-09-02 06:20:12 +0530

    added comments

--- Band_name_generator.py
+++ Band_name_generator.py
@@ -1,4 +1,9 @@
+#TAKING INPUT FROM USER
+
 print("Welcome to the Band Name Generator!")
 city = input("What's name of your city you gre up in?\n")
 pet = input("What's your pet's name?\n")
+
+#CONCATENATE A STRING
+
 print("Your band name could be "+city+" "+pet+"!")
